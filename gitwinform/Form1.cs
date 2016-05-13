086d00b3e3032eb63a745a3299fb25a55d678fef
thinktree <thinktree@DESKTOP-37JR0MG>
2016-05-13 09:53:34 +0900

    print 메서드 재 추가

--- gitwinform/Form1.cs
+++ gitwinform/Form1.cs
@@ -12,9 +12,16 @@ namespace gitwinform
 {
     public partial class Form1 : Form
     {
+    
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void print()
+        {
+            MessageBox.Show("print~");
+        }
+
     }
 }
